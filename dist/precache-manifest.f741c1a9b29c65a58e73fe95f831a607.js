3ab8bbcf2068862c5574b2538b87c2169be0f38e
dk880842 <dk880842@gmail.com>
2021-12-28 17:52:06 +0800

    更新

---
+++ dist/precache-manifest.f741c1a9b29c65a58e73fe95f831a607.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01f1aeabc607989ac971898bf8e6ba0",
+    "url": "index.html"
+  },
+  {
+    "revision": "7c90482c96d4695716c8",
+    "url": "static/css/FundingRate.fd674df0.css"
+  },
+  {
+    "revision": "40f737a4643388d89490",
+    "url": "static/css/Home.835372c9.css"
+  },
+  {
+    "revision": "60e15811ff97d8afe4d8",
+    "url": "static/css/Income.96273ea8.css"
+  },
+  {
+    "revision": "864cb80cb9c40ed3de23",
+    "url": "static/css/Login.82c8ea3e.css"
+  },
+  {
+    "revision": "26610406e928b8965188",
+    "url": "static/css/PremiumIndex.aefd3fcc.css"
+  },
+  {
+    "revision": "47c156ce15d120fed926",
+    "url": "static/css/app.98de5356.css"
+  },
+  {
+    "revision": "6cf190084ace2d79b130",
+    "url": "static/css/vendor.6e808e7d.css"
+  },
+  {
+    "revision": "3923d35243e344c75358b42fad31a959",
+    "url": "static/image/BanktoBank.png"
+  },
+  {
+    "revision": "61fca63109943b8df93a",
+    "url": "static/js/Dashboard.61fca631.js"
+  },
+  {
+    "revision": "7c90482c96d4695716c8",
+    "url": "static/js/FundingRate.7c90482c.js"
+  },
+  {
+    "revision": "40f737a4643388d89490",
+    "url": "static/js/Home.40f737a4.js"
+  },
+  {
+    "revision": "60e15811ff97d8afe4d8",
+    "url": "static/js/Income.60e15811.js"
+  },
+  {
+    "revision": "864cb80cb9c40ed3de23",
+    "url": "static/js/Login.864cb80c.js"
+  },
+  {
+    "revision": "61317d8e69af7ea61551",
+    "url": "static/js/NotFound.61317d8e.js"
+  },
+  {
+    "revision": "26610406e928b8965188",
+    "url": "static/js/PremiumIndex.26610406.js"
+  },
+  {
+    "revision": "47c156ce15d120fed926",
+    "url": "static/js/app.47c156ce.js"
+  },
+  {
+    "revision": "6ae615bfbbad26c09ea2",
+    "url": "static/js/manifest.6ae615bf.js"
+  },
+  {
+    "revision": "6cf190084ace2d79b130",
+    "url": "static/js/vendor.6cf19008.js"
+  },
+  {
+    "revision": "07e6a851297485734c8961d30858347e",
+    "url": "static/readme.txt"
+  }
+]);
---
+++ dist/sw.js
@@ -0,0 +1,48 @@
+importScripts("precache-manifest.f741c1a9b29c65a58e73fe95f831a607.js");
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+try {
+    workbox.setConfig({
+        debug: false
+    })
+    workbox.core.clientsClaim()
+    workbox.core.skipWaiting()
+    const cacheableResponse = new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+    })
+    const jsHandler = new workbox.strategies.CacheFirst({
+        cacheName: 'js-cache',
+        plugins: [
+            cacheableResponse,
+            new workbox.expiration.Plugin({
+                maxEntries: 100
+            })
+        ]
+    })
+    workbox.routing.registerRoute(/.*.js/, jsHandler)
+
+    const cssHandler = new workbox.strategies.CacheFirst({
+        cacheName: 'css-cache',
+        plugins: [
+            cacheableResponse,
+            new workbox.expiration.Plugin({
+                maxEntries: 100
+            })
+        ]
+    })
+    workbox.routing.registerRoute(/.*.css/, cssHandler)
+
+    const imgHandler = new workbox.strategies.CacheFirst({
+        cacheName: 'image-cache',
+        plugins: [
+            cacheableResponse,
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 24 * 60 * 60
+            })
+        ]
+    })
+    workbox.routing.registerRoute(/.*.(?:png|jpg|jpeg|svg|gif|ico)/, imgHandler)
+} catch (err) {
+    console.error(err)
+}
+
